f8daea51207aa067c6d80a72548536f64cc63265
gunitkandhway <110780427+gunitkandhway@users.noreply.github.com>
2025-03-07 12:17:04 +0530

    Added the music data

---
+++ src/data/musicData.js
@@ -0,0 +1,60 @@
+export const musicData = [
+    {
+      title: "Starboy",
+      artistName: "The Weeknd",
+      thumbnail: "https://i.scdn.co/image/ab67616d0000b273a048415db06a5b6fa7ec4e1a",
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+      duration: 256, 
+    },
+    {
+      title: "Demons",
+      artistName: "Imagine Dragons",
+      thumbnail: "https://i.scdn.co/image/ab67616d0000b273727a2e1bef7c32f3d0e89524",
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+      duration: 324, 
+    },
+    {
+      title: "Mouth of the river",
+      artistName: "Imagine Dragons",
+      thumbnail: "https://i.scdn.co/image/ab67616d0000b273727a2e1bef7c32f3d0e89524",
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
+      duration: 383, 
+    },
+    {
+      title: "Ghost Stories",
+      artistName: "Coldplay",
+      thumbnail: "https://i.scdn.co/image/ab67616d0000b273f864bcdcc245f06831d17ae0",
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3",
+      duration: 190, 
+    },
+    {
+      title: "Sparks",
+      artistName: "Coldplay",
+      thumbnail: "https://i.scdn.co/image/ab67616d0000b273f864bcdcc245f06831d17ae0",
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3",
+      duration: 263, 
+    },
+    {
+      title: "Viva La Vida",
+      artistName: "Coldplay",
+      thumbnail: "https://i.scdn.co/image/ab67616d0000b2734e0362c225863f6ae2432651",
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3",
+      duration: 332, 
+    },
+    {
+      title: "Hymn for the weekend",
+      artistName: "Coldplay",
+      thumbnail: "https://i.scdn.co/image/ab67616d0000b2738ff7c3580d429c8212b9a3b6",
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3",
+      duration: 153, 
+    },
+    {
+      title: "Pain",
+      artistName: "Ryan Star",
+      thumbnail: "https://i.scdn.co/image/ab67616d0000b273b5db31a7c2a1a5a5e3b4c4f5",
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3",
+      duration: 192, 
+    },
+  ]
+  
+  
